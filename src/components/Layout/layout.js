582300c5c832099594ab35e50eba375bf3b3b9e9
Mariusz Mańka <mmanka@livocloud.pl>
2021-07-13 20:59:01 +0200

    Bug fixes and refactor

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import ThemeProvider from '../Theme/ThemeProvider'
+import ThemeProvider from '../../Theme/ThemeProvider'
 
 import "./layout.css"
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,10 @@
-import { graphql } from 'gatsby'
 import * as React from "react"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout/layout"
 import Seo from "../components/seo"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import WaitingForPage from './WaitingForPage/WaitingForPage'
+import WaitingForPage from '../components/WaitingForPage/WaitingForPage'
 
-const IndexPage = ({ data }) => {
-  const image = getImage(data.file)
-
+const IndexPage = () => {
   return (
     <Layout>
       <Seo title="Home" />
@@ -17,15 +13,5 @@ const IndexPage = ({ data }) => {
   )
 }
 
-export const imageQuery = graphql`
-query imageQuery {
-  file(name: {eq: "hero"}, childImageSharp: {fixed: {}}) {
-    childImageSharp {
-      	 gatsbyImageData(layout: FIXED)
-    }
-  }
-}
-
-`
 
 export default IndexPage
